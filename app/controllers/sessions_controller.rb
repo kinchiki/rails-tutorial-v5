35a90a6034845da15fb955fb96081cf6fd6e95ba
Akito Naito <naito.akito@gmail.com>
2022-10-28 22:36:15 +0900

    add create action and test of sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(Params[:session][:password])
+      # redirect users show
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destory
+  end
 end
