35e7356d5522d9a49d099789b00b511980c92217
Akito Naito <naito.akito@gmail.com>
2022-10-28 23:25:18 +0900

    implement login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "¡Hola, mundo!"
-  end
-
-  def goodbye
-    render html: "goodbye, world!"
-  end
+  include SessionsHelper
 end
