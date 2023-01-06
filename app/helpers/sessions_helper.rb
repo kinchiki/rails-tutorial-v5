4feb777c989817d04e92f8539a0fcf7d56fea127
Akito Naito <naito.akito@gmail.com>
2023-01-06 22:21:36 +0900

    add comment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,17 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -21,10 +24,12 @@ module SessionsHelper
     end
   end
 
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     current_user.present?
   end
 
+  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
