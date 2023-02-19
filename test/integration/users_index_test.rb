603258724cd33abefc8d1aed3a6ec5dc1547c729
Akito Naito <naito.akito@gmail.com>
2023-02-19 15:45:54 +0900

    add test of not display non-activated-user

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,14 +11,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
+      assert_equal user.activated, true
       assert_select 'a[href=?]', user_path(user), text: user.name
       assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
     end
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
+  end
+
+  test "show not display non-activated" do
+    log_in_as(@non_admin)
+    non_activated_user = users('non-active')
+    get user_path(non_activated_user)
+    follow_redirect!
+    assert_template 'static_pages/home'
   end
 
   test "index as non-admin" do
